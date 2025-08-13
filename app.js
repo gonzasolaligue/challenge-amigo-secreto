9544fb8b1b247dda9e207642b3ca7f7ee93ca039
gonzasolaligue <gonzasolaligue@gmail.com>
2025-08-13 12:39:55 -0300

    Se agrega array, funcion para agregar amigos, y mostrar la lista en la página

--- app.js
+++ app.js
@@ -1,1 +1,32 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = []
+let inputAmigo = document.getElementById('amigo');
+let listaAmigos = document.getElementById('listaAmigos');
+let buttonAdd = document.getElementById('button-add');
+
+
+function agregarAmigo() {
+    let nombre = inputAmigo.value;
+
+    if (nombre === "") {
+        alert("Por favor, inserte un nombre.");
+        return; //devuelve si no es válido
+    }
+
+    amigos.push(nombre);
+    inputAmigo.value = "";
+    mostrarLista()
+}
+
+console.log(amigos)
+
+function mostrarLista() {
+    listaAmigos.innerHTML = ""; //se limpia la lista
+
+    for (let i = 0; i < amigos.length; i++) {
+        listaAmigos.innerHTML += "<li>" + amigos[i] + "</li>";
+    }
+}
+
+//conecto el boton con la funcion
+buttonAdd.addEventListener('click', agregarAmigo);
+
